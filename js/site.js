4c39355152b2b23fa1877c19894be546f6b4bdcd
Ryan Doerr <ryan.doerr15@gmail.com>
2023-04-17 12:09:08 -0400

    edited code

--- js/site.js
+++ js/site.js
@@ -2,42 +2,72 @@
 function getValues() {
   //get the user's input
   //decide what to do with it
+
   let userInput = document.getElementById("message").value;
 
-  let reversedInput = checkForPalindrome(
-    userInput.toLowerCase().replaceAll(" ", "")
-  );
+  if (userInput.length > 0) {
+    let reversedInput = reverseString(userInput);
+    let isPalindrome = checkForPalindrome(userInput);
 
-  displayResults(reversedInput);;
+    displayResult(reversedInput, isPalindrome);
+  } else {
+    Swal.fire({
+      icon: "error",
+      backdrop: false,
+      title: "Taco Cat",
+      text: "You must enter an input to check for palindrome!",
+    });
+  }
+}
+
+function checkForPalindrome(userString) {
+  userString = userString.toLowerCase();
+
+  const regex = /[^a-z0-9]/gi;
+  userString = userString.replace(regex, "");
+
+  let reversedString = reverseString(userString);
+
+  let isPalindrome = reversedString == userString;
+
+  return isPalindrome;
 }
 
 //business/logic function
-function checkForPalindrome(input) {
+function reverseString(message) {
   //take a string, return it in reverse
-  let inputLength = input.length;
+  let reversedMsg = "";
 
-  for (let i = 0; i < inputLength / 2; i++) {
-    if (input[i] !== input[inputLength - 1 - i]) {
-      return '';
-    }
+  for (let i = message.length - 1; i >= 0; i--) {
+    reversedMsg += message[i];
   }
-  return input;
+
+  //reversedMsg = message.split('').reverse().join(''); quick solution
+
+  return reversedMsg;
 }
 
 //view function
-function displayResults(revInput) {
+function displayResult(resultMessage, isPalindrome) {
   //show the string on the page
-  if (revInput) {
-    document.getElementById('msg').textContent = revInput;
+  let resultMessage = "";
+
+  let alertClass = isPalindrome ? "alert-success" : "alert-danger";
+  alertBox.classList.add(alertClass);
+
+  if (isPalindrome) {
+    resultMessage = "You enetred a palindrome!";
   } else {
-    document.getElementById('msg2').textContent = revInput;
+    resultMessage = "You did not enter a plaindrome!";
   }
 
-  if (revInput) {
-    document.getElementById("alert").classList.remove("d-none");
-    document.getElementById("alert2").classList.add("d-none");
-  } else {
-    document.getElementById("alert2").classList.remove("d-none");
-    document.getElementById("alert").classList.add("d-none");
-  }
+  resultMessage += "your message reversed is" + resultMessage;
+
+  document.getElementById("msg").textContent = resultMessage;
+  document.getElementById("alert").remove("d-none");
+
+  let alertBox = document.getElementById("alert");
+  alertBox.classList.remove("alert-danger");
+  alertBox.classList.remove("alert-success");
+  alertBox.classList.add("d-none");
 }
